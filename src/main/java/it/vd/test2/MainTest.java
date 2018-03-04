53eceaa6879459f1f6e3cc93683fcc45959936b9
Master-PC\Master <u1484032@mvrht.net>
2018-03-04 18:04:57 +0100

    Squashed commit of the following:
    commit 8c010676f0d4f2871786e003bd7ac68d52e2e379
    Author: Master-PC\Master <u1484032@mvrht.net>
    Date:   Sun Mar 04 18:01:42 2018 +0100
    
    	inserito il commento sull'istruzione di stampa
    
    commit cfdc1f3aabbd4b69ed796af4f229c29df8c780eb
    Author: Master-PC\Master <u1484032@mvrht.net>
    Date:   Sun Mar 04 17:59:43 2018 +0100
    
    	inserito commento entry point
    
    commit 14ca5bf3a53f54a368542eac19f2bfc9179515d9
    Author: Master-PC\Master <u1484032@mvrht.net>
    Date:   Sun Mar 04 17:59:17 2018 +0100
    
    	inserimento autore
    
    commit a3fa705b3ddbe57747407e6cfa7bdacf3d5491d9
    Author: Master-PC\Master <u1484032@mvrht.net>
    Date:   Sun Mar 04 17:57:35 2018 +0100
    
    	creazione del progetto

---
+++ src/main/java/it/vd/test2/MainTest.java
@@ -0,0 +1,15 @@
+package it.vd.test2;
+
+/*
+ * autore Valerio Doddi
+ */
+public class MainTest {
+	
+	//metodo main entry point
+	public static void main(String[] args) {
+		//inserita la stampa a video
+		System.out.println("Hello world!");
+		
+	}
+
+}
